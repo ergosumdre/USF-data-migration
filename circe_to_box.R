6d40821756ebbc6f20f26d3a24ea39ee0d6c16ab
ergosumdre <35677602+ergosumdre@users.noreply.github.com>
2021-05-31 12:40:49 -0400

    Update circe_to_box.R

--- circe_to_box.R
+++ circe_to_box.R
@@ -4,19 +4,15 @@ library("dplyr")
 
 # setwd()
 # box auth
-box_auth(client_id = "XXXXXXXXXXXXXX", 
-         client_secret = "XXXXXXXXXXXXX")
+boxr::box_auth(client_id = "XXXXXXXXX", 
+         client_secret = "XXXXXXXXX")
 
 # get circe dir str
 box_dir <- substr(getwd(), start = nchar(getwd()) - 6, stop = nchar(getwd()))
 
 # create box dir with circe dir str
-boxr::box_dir_create(dir_name = box_dir, parent_dir_id = "XXXXXXXXX")
-
-# searches for new box dir and sets wd
-new_id <- box_search(box_dir, type = "folder") %>% as.data.frame() %>% dplyr::select(id,name) %>% filter(name == box_dir) %>% select(id)
-new_id <- as.character(new_id$id)
-boxr::box_setwd(new_id)
+box.d <- boxr::box_dir_create(dir_name = box_dir, parent_dir_id = "XXXXXXXXX")
 
 # upload local dir to box in new dir
-box_push(dir_id = new_id, local_dir = getwd(),ignore_dots = TRUE,overwrite = FALSE,delete = FALSE)
+boxr::box_push(dir_id = box.d[2], local_dir = getwd(),ignore_dots = TRUE,overwrite = FALSE,delete = FALSE)
+
